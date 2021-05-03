07294068d25d985592da5883bd0ca38a64740fb9
Taylor Anderson <taylor@Taylors-MacBook-Pro.local>
2021-05-03 18:55:25 -0500

    adjusted styling on 404 and thank you pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,11 +9,11 @@ const ErrorPage = () => (
     <div className="bg-gray-100">
       <div className="container py-12 pb-64 lg:pb-128">
         <div className="w-full md:w-1/2 xl:w-3/5 pb-8 md:pb-0">
-          <h1 className="text-3xl leading-tight font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h1 className="text-3xl leading-tight font-extrabold tracking-tight text-gray-800 sm:text-4xl mb-6">
             Error 404
           </h1>
 
-          <h2 className="text-xl leading-tight font-semibold tracking-tight text-blue-600 sm:text-2xl">
+          <h2 className="text-xl font-semibold text-gray-600 sm:text-2xl">
             The page doesn't exists.
           </h2>
         </div>
