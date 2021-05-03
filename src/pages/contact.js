76bb1351d2c28eca8c95ca2c63adeeb8bd35c63a
Taylor Anderson <taylor@Taylors-MacBook-Pro.local>
2021-05-03 18:19:58 -0500

    fix form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,7 +21,7 @@ function ContactPage() {
 
                 <form className="mx-auto w-full" method="post" netlify-honeypot="bot-field" name="contact" data-netlify="true">
                   <input type="hidden" name="bot-field" />
-                  <input type="hidden" name="contact" value="contact" />
+                  <input type="hidden" name="form-name" value="contact" />
 
                   <label
                     className="block mb-2 font-bold text-gray-800 text-xl"
